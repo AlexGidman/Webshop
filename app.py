3c7ca0979d694c9a41549291297d6b4c3032ebf1
Alex Gidman <alexgidman@outlook.com>
2020-10-05 11:27:33 +0100

    substitute in wtforms

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request, redirect, url_for, g
+from flask import Flask, render_template, request, redirect, url_for, g, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
 # import pdb
 import sqlite3
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "dev"
 
+class NewItemForm(FlaskForm):
+    title       = StringField("Title")
+    price       = StringField("Price")
+    description  = TextAreaField("Description")
+    submit      = SubmitField("Submit")
 
 @app.route("/")
 def home():
     conn = get_db()
     c = conn.cursor()
+
     items_from_db = c.execute("""SELECT
                     i.id, i.title, i.description, i.price, c.name, s.name
                     FROM
@@ -40,22 +49,24 @@ def new_item():
     # pdb.set_trace() # stops package here to inspect post request
     conn = get_db()
     c = conn.cursor()
+    form = NewItemForm()
 
     if request.method == "POST":
         c.execute("""INSERT INTO items 
         (title, description, price, image, category_id, subcategory_id) 
         VALUES (?,?,?,?,?,?)""",
-                  (request.form.get("title"),
-                   request.form.get("description"),
-                   float(request.form.get("price")),
+                  (form.title.data,
+                   form.description.data,
+                   float(form.price.data),
                    "",
                    1,
                    1))
         conn.commit()
 
+        flash("Item {} has been successfully submitted".format(request.form.get("title")), "success")
         return redirect(url_for("home"))
     else:
-        return render_template("new_item.html")
+        return render_template("new_item.html", form=form)
 
 
 def get_db():
