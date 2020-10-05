5481f7f161710df47b87c521def74241a2ae7009
Alex Gidman <alexgidman@outlook.com>
2020-10-05 09:31:04 +0100

    add_new_item route + db initialise & query app

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import pdb
+import 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/item/new", methods=["GET", "POST"])
+def new_item():
+    # pdb.set_trace() # stops package here to inspect post request
+    if request.method == "POST":
+        return redirect(url_for("home"))
+    else:
+        return render_template("new_item.html")
